2e750a9cb94d85692db4eb71ae86d8cc1497cc9f
kleinerELM <mail@floriankleiner.de>
2025-01-02 22:22:24 +0100

    added own toolbar, added prev/next-buttons

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,7 @@
-var getUrlParameter = function getUrlParameter(sParam) {
+let file_data;
+var viewer = null;
+
+function getUrlParameter(sParam) {
 	var sPageURL = window.location.search.substring(1),
 		sURLVariables = sPageURL.split('&'),
 		sParameterName,
@@ -29,7 +32,7 @@ function get_id( data ) {
 	}
 }
 
-function build_navigation( path, data ) {
+function build_navigation( path, data, viewer ) {
 	var items = [];
 	for (i = 0; i < data.length; ++i) {
 		var id = data[i];
@@ -45,35 +48,34 @@ function build_navigation( path, data ) {
 		// set the onclick funktion of the image
 		$("#link_" + data[i]).click(function(){
 			var id = this.id.split("_")[1]; // get id from html-block-id
-			preview_click_evt(id, path, file_data);
+			preview_click_evt(id, path, file_data, viewer);
 		});
 	}
 }
 
-function load_openseadragon_viewer( path, id, degrees, overlays ) {
+function load_openseadragon_viewer( path, id, degrees ) {
 	var viewer = OpenSeadragon({
 		id: "openseadragon1",
-		prefixUrl: "./images/",
+		//prefixUrl: "./images/",
 		tileSources: path + id + "/stack.dzi", // Pfad zur DZI-Datei
-
 		overlays: {
 			id: 'example-overlay',
 			x: 0.33,
 			y: 0.75,
-			width: 0.2,
-			height: 0.25,
+			width: 2000,
+			height: 1000,
 			className: 'highlight'
 		},
 
-		showNavigator: true, // Navigator-Minimap anzeigen
-		navigatorPosition: "BOTTOM_RIGHT", // Position der Minimap
-		navigatorSizeRatio:0.15,
-		navigatorWidth:"15em",
-		navigatorHeight:"15em",
-		navigatorAutoFade:true,
-		preserveViewport: true,
-		degrees: degrees,
-		visibilityRatio: 1,
+		showNavigator:      true, // Navigator-Minimap anzeigen
+		navigatorPosition:  "BOTTOM_RIGHT", // Position der Minimap
+		navigatorSizeRatio: 0.15,
+		navigatorWidth:     "15em",
+		navigatorHeight:    "15em",
+		navigatorAutoFade:  true,
+		preserveViewport:   false,
+		degrees:            degrees,
+		visibilityRatio:   1,
 		minZoomImageRatio: 1,
 		zoomInButton:   "zoom-in",
 		zoomOutButton:  "zoom-out",
@@ -81,6 +83,7 @@ function load_openseadragon_viewer( path, id, degrees, overlays ) {
 		fullPageButton: "full-page",
 		/* showSequenceControl: true,*/
 	});
+	return viewer;
 }
 
 function map_click_evt( map_open ) {
@@ -95,7 +98,8 @@ function map_click_evt( map_open ) {
 	$("#nav").animate({"margin-bottom": map_open ? '20.5rem' : '4rem'}, 500);
 }
 
-function preview_click_evt(id, path, file_data) {
+var last_item = false;
+function preview_click_evt(id, path, file_data, viewer ) {
 	$("#head>span").animate({opacity:0},100);
 	var map_image = ("map" in file_data[id]) ?
 		'media/' + file_data[id]["map"] :
@@ -104,10 +108,27 @@ function preview_click_evt(id, path, file_data) {
 		file_data[id]["name"] :
 		id;
 
-	//window.location.href = '?id=' + this.id.substr(this.id.length - 4); // Ziel-URL hier anpassen
-	$('#openseadragon1').empty();
 	$('#head_id').text(name);
-	load_openseadragon_viewer( path, id, file_data[id]["deg"] );
+	if (last_item === false) last_item = viewer.world.getItemAt(0);
+
+	viewer.world.removeItem(last_item); // delete last image
+	// load new source
+	viewer.addTiledImage({
+		tileSource: path + id + "/stack.dzi",
+		degrees: file_data[id]["deg"],
+		success: function (obj) { last_item = obj.item; },
+		overlays: {
+			id: 'example-overlay',
+			x: 0.33,
+			y: 0.75,
+			width: 2000,
+			height: 1000,
+			className: 'highlight'
+		},
+	});
+
+	update_prev_next_btn( file_data, id, path, viewer );
+
 	$("#head span").animate({'opacity':1},1000);
 
 	if ( $('#map_nav').length ) {
@@ -122,16 +143,17 @@ function preview_click_evt(id, path, file_data) {
 	}
 }
 
-function init_page( path, std_map_url) {
+function init_page( path, std_map_url, viewer) {
 
 	$.getJSON( path + "files.json", function( data ) {
 		file_data = data;
 		file_list = Object.keys(file_data);
-		build_navigation( path, file_list );
 		var id = get_id( file_list );
 
 		$('#head_id').text(id);
-		load_openseadragon_viewer( path, id, file_data[id]["deg"] );
+		viewer = load_openseadragon_viewer( path, id, file_data[id]["deg"] );
+		build_navigation( path, file_list, viewer );
+		update_prev_next_btn( file_data, id, path, viewer );
 		$("#head span").animate({opacity:1},1000);
 		document.getElementById('link_' + id).scrollIntoView();
 
@@ -165,11 +187,44 @@ function map_hinting( time ) {
     }, time );
 };
 
+function update_prev_next_btn( file_data, id, path, viewer ) {
+	file_list = Object.keys(file_data);
+	var prev_id = false;
+	var next_id = false;
+	for (i = 0; i < file_list.length; ++i) {
+		if ( id == file_list[i] ) {
+			if ( i > 0 ) prev_id = file_list[i-1];
+			if ( i+1 < file_list.length ) next_id = file_list[i+1];
+		}
+	}
+	$("#previous").off();
+	if ( prev_id ) {
+		$("#previous").css({'opacity':1});
+		$("#previous").click(function() {
+			document.getElementById('link_' + prev_id).scrollIntoView();
+			preview_click_evt(prev_id, path, file_data, viewer);
+		});
+	} else {
+		$("#previous").css({'opacity':0.1});
+	}
+
+	$("#next").off();
+	if ( next_id ) {
+		$("#next").css({'opacity':1});
+		$("#next").click(function() {
+			document.getElementById('link_' + next_id).scrollIntoView();
+			preview_click_evt(next_id, path, file_data, viewer);
+		});
+	} else {
+		$("#next").css({'opacity':.1});
+	}
+}
+
 $(document).ready(function() {
-	let file_data;
 
 	// load json and the respecting images
-	init_page( path, std_map_url );
+	//$('#toolbar_container').load('toolbar_container.html');
+	init_page( path, std_map_url, viewer );
 	if ( $('#map_nav').length ) {
 		$("#map_nav").prop('open', 1);
 
